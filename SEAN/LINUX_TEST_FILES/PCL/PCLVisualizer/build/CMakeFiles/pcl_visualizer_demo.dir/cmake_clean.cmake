339cdffe9839dfdcf53143d600606fdd36c0b96c
Sean Hendrickson <hsean@pdx.edu>
2016-01-12 19:52:43 -0800

    My Ubuntu Test Files
    Adding a folder to store my collection of tutorial code and stuff I am
    playing with

---
+++ SEAN/LINUX_TEST_FILES/PCL/PCLVisualizer/build/CMakeFiles/pcl_visualizer_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_visualizer_demo.dir/pcl_visualizer_demo.cpp.o"
+  "pcl_visualizer_demo.pdb"
+  "pcl_visualizer_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_visualizer_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ SEAN/LINUX_TEST_FILES/PCL/PCL_read_example/build/CMakeFiles/pcd_read.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcd_read.dir/pcd_read.cpp.o"
+  "pcd_read.pdb"
+  "pcd_read"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcd_read.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
