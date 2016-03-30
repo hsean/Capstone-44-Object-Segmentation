f6f25fd0e75ba3a46e8d362ebedd909053d58a69
sarojbardewa <saroj@pdx.edu>
2016-03-30 13:37:10 -0700

    Added QT files

---
+++ SAROJ/QT1/build/CMakeFiles/pcl_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "ui_pclviewer.h"
+  "moc_pclviewer.cxx"
+  "CMakeFiles/pcl_visualizer.dir/main.cpp.o"
+  "CMakeFiles/pcl_visualizer.dir/pclviewer.cpp.o"
+  "CMakeFiles/pcl_visualizer.dir/moc_pclviewer.cxx.o"
+  "pcl_visualizer.pdb"
+  "pcl_visualizer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
