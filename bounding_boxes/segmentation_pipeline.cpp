5db46fae54cd82ccc01e29b298cf93fb935d454a
Matt Whiteside <matt.whiteside@gmail.com>
2016-04-10 22:35:39 -0700

    Adding demo visualizing height calculation for camera.

--- bounding_boxes/segmentation_pipeline.cpp
+++ bounding_boxes/segmentation_pipeline.cpp
@@ -14,37 +14,29 @@ SegmentationPipeline::SegmentationPipeline(Cloud3D::Ptr rawCloud,
                                            float voxelSize,
                                            float sampleSize,
                                            float stdDev,
-                                           float iterationDivisor,
-                                           bool downsample) :
+                                           float iterationDivisor) :
 	tree(new search::KdTree<PointXYZ>()),
-	cloud_normals(new PointCloud<Normal>),
-  normalsMinusObjectsAndPlane(new PointCloud<Normal>),
-  cloudMinusObjectsAndPlane(new Cloud3D),
+  normals(new PointCloud<Normal>),
+  objectCloud(new Cloud3D),
   denoisedCloud(rawCloud),
   iterationDivisor(iterationDivisor),
-  convexHull (new Cloud3D)
+  convexHull (new Cloud3D),
+  planeCloud(new Cloud3D),
+  sampleSize(sampleSize),
+  stdDev(stdDev)
 {
  
 	
-//	pass.setInputCloud(rawCloud);
-//  unsigned nonNaNCount = 0;
-//  for (PointXYZ p :  (*rawCloud).points){
-//    if (!isnan(p.z)){
-//      nonNaNCount++;
-//      std::cout << p << std::endl;
-//    }
-//  }
+
 	
   cutoffZ(denoisedCloud,denoisedCloud);
-  //removeStatOutliers(cloudMinusObjectsAndPlane, cloudMinusObjectsAndPlane);
   
-  removeNoise(denoisedCloud,
-              denoisedCloud,
-              sampleSize, stdDev);
-  //downsample(cloudMinusObjectsAndPlane, cloudMinusObjectsAndPlane, 0.005);
-  if (downsample){
-    voxelFilter(denoisedCloud, denoisedCloud, voxelSize);
-  }
+
+
+  
+  voxelFilter(denoisedCloud, denoisedCloud, voxelSize);
+  
+  
 	NormalEstimation<PointXYZ, Normal> ne;
 
   std::cerr << "PointCloud after filtering has: " <<
@@ -55,22 +47,19 @@ SegmentationPipeline::SegmentationPipeline(Cloud3D::Ptr rawCloud,
 	
 	ne.setSearchMethod(tree);
 	ne.setInputCloud(denoisedCloud);
-	ne.setKSearch(50);
-	ne.compute(*normalsMinusObjectsAndPlane);
+	ne.setKSearch(25);
+	ne.compute(*normals);
 
   
 }
 
 bool SegmentationPipeline::performSegmentation(){
   if (extractPrism()){
-    
     int max = 1;
     bool stillFindingStuff = true;
+    //this is to support extracting multiple objects
     do {
       stillFindingStuff = extractGraspableObject(SACMODEL_CYLINDER);
-//      if (graspableObjects.size() < max){
-//        extractPlane();
-//      }
     } while (stillFindingStuff && graspableObjects.size() < max);
     return true;
   } else {
@@ -78,96 +67,72 @@ bool SegmentationPipeline::performSegmentation(){
   }
 }
 
+bool SegmentationPipeline::extractPlane()
+{
+  planeCoefficients = ModelCoefficients::Ptr(new ModelCoefficients);
+  PointIndices::Ptr plane_indices(new PointIndices);
+  
+  
+  ExtractIndices<PointXYZ> extractor;
+  
+  // Create the segmentation object for the planar
+  // model and set all the parameters
+  seg.setOptimizeCoefficients (true);
+  seg.setModelType(SACMODEL_NORMAL_PLANE);
+  seg.setNormalDistanceWeight (0.1);
+  
+  seg.setMethodType(SAC_RANSAC);
+  int plane_iterations = int(ceil(float(100)/iterationDivisor));
+  seg.setMaxIterations(plane_iterations);
+  seg.setDistanceThreshold (0.03);
+  seg.setInputCloud (denoisedCloud);
+  seg.setInputNormals (normals);
+  // Obtain the plane inliers and coefficients
+  seg.segment(*plane_indices, *planeCoefficients);
+  std::cerr << "Plane coefficients: " << *planeCoefficients << std::endl;
+  
+  // Extract the planar inliers from the input cloud
+  extractor.setInputCloud (denoisedCloud);
+  extractor.setIndices (plane_indices);
+
+  extractor.filter (*planeCloud);
+  return planeCloud->points.size() > 0;
+}
+
 
 bool SegmentationPipeline::extractPrism()
 {
-//  int result = c44::getPrism(originalCloud, cloudMinusObjectsAndPlane,
-//                             0, 1.57, 100, 0.01, 0.02, 0.2);
-//  return result == 0;
-
-  ModelCoefficients::Ptr coefficients_plane (new ModelCoefficients);
-	PointIndices::Ptr plane_indices(new PointIndices);
-	
-	
-	ExtractIndices<PointXYZ> extract;
-	
-	
-	// Estimate point normals
-	
-	// Create the segmentation object for the planar
-	// model and set all the parameters
-	seg.setOptimizeCoefficients (true);
-	seg.setModelType(SACMODEL_NORMAL_PLANE);
-	seg.setNormalDistanceWeight (0.1);
   
-	seg.setMethodType(SAC_RANSAC);
-  int plane_iterations = int(ceil(float(100)/iterationDivisor));
-	seg.setMaxIterations(plane_iterations);
-	seg.setDistanceThreshold (0.03);
-	seg.setInputCloud (denoisedCloud);
-	seg.setInputNormals (normalsMinusObjectsAndPlane);
-	// Obtain the plane inliers and coefficients
-	seg.segment (*plane_indices, *coefficients_plane);
-	std::cerr << "Plane coefficients: " << *coefficients_plane << std::endl;
-
-	// Extract the planar inliers from the input cloud
-	extract.setInputCloud (denoisedCloud);
-	extract.setIndices (plane_indices);
-  //extract.setNegative (false);
-
-	pcl::PointCloud<PointXYZ>::Ptr planeCloud(new pcl::PointCloud<pcl::PointXYZ> ());
-	extract.filter (*planeCloud);
-	std::cerr << "PointCloud representing the planar component: " <<
-							 planeCloud->points.size () <<
-							 " data points." <<
-							 std::endl;
-	
-  
-  // Extract the planar inliers from the input cloud
-  if (planeCloud->points.size() > 0){
-    //this->plane = new Plane(*coefficients_plane,cloud_plane,cloud_normals,inliers);
+  if (extractPlane()){
     
-    
-    // Remove the planar inliers, extract the rest
-    
-    
-    
-    pcl::ConvexHull<pcl::PointXYZ> chull;
+    pcl::ConvexHull<pcl::PointXYZ> chull;    
     chull.setInputCloud (planeCloud);
     chull.setDimension(2);
     chull.reconstruct(*convexHull);
     
       
-    // segment those points that are in the polygonal prism
-    pcl::ExtractPolygonalPrismData<pcl::PointXYZ> prism;
-    prism.setInputCloud(denoisedCloud);
-    prism.setInputPlanarHull(convexHull);
-    //0, 1.57, 1000, 0.01, 0.02, 0.2
-    prism.setHeightLimits(0.02, 0.2);
+    //segment out those points that are in the polygonal prism
+    pcl::ExtractPolygonalPrismData<pcl::PointXYZ> prismExtractor;
+    prismExtractor.setInputCloud(denoisedCloud);
+    prismExtractor.setInputPlanarHull(convexHull);
+    prismExtractor.setHeightLimits(0.02, 0.2);
     pcl::PointIndices::Ptr objectIndices (new pcl::PointIndices);
-    prism.segment (*objectIndices);
+    prismExtractor.segment (*objectIndices);
     
-    // get all points retrieved by the hull
-    extract.setInputCloud(denoisedCloud);
-    extract.setIndices(objectIndices);
-    cloudMinusObjectsAndPlane.reset(new Cloud3D);
-    extract.filter(*cloudMinusObjectsAndPlane);
     
-    ExtractIndices<Normal> extract_normals;
-    //extract_normals.setNegative (true);
-    extract_normals.setInputCloud (normalsMinusObjectsAndPlane);
-    extract_normals.setIndices(objectIndices);
-    extract_normals.filter (*normalsMinusObjectsAndPlane);
+    //remove the convex-hull's points from the main cloud
+    ExtractIndices<PointXYZ> extractor;
+    extractor.setInputCloud(denoisedCloud);
+    extractor.setIndices(objectIndices);
+    extractor.filter(*objectCloud);
+    
+    ExtractIndices<Normal> normalExtractor;
+    normalExtractor.setInputCloud (normals);
+    normalExtractor.setIndices(objectIndices);
+    normalExtractor.filter (*normals);
 
-    
-    // check if any objects were found
-    if(0 == cloudMinusObjectsAndPlane->points.size())
-    {	// no objects found
-      return false;
-    }
-    
 
-    return true;
+    return objectCloud->points.size() > 0;
   } else{
     return false;
   }
@@ -177,53 +142,66 @@ bool SegmentationPipeline::extractPrism()
 bool SegmentationPipeline::extractGraspableObject(SacModel model)
 {
 	ModelCoefficients::Ptr objectCoefficients(new ModelCoefficients);
-	ExtractIndices<PointXYZ> extract;
-	ExtractIndices<Normal> extract_normals;
+	ExtractIndices<PointXYZ> extractor;
+	ExtractIndices<Normal> normalExtractor;
 	
 	PointIndices::Ptr inliers(new PointIndices);
   PointCloud<Normal>::Ptr objectNormals(new PointCloud<Normal>);
   Cloud3D::Ptr cylinderCloud(new Cloud3D);
-	
-  //cloudMinusObjectsAndPlane.reset(&(*cloudMinusObjectsAndPlane));
+  Cloud3D::Ptr denoisedObjectCloud(objectCloud);
+  
+  removeNoise(denoisedObjectCloud,
+              denoisedObjectCloud,
+              sampleSize, stdDev);
+  
+
+
+  normals.reset(new PointCloud<Normal>);
+  
+  NormalEstimation<PointXYZ, Normal> ne;
+  
+  std::cerr << "PointCloud after filtering has: " <<
+  denoisedCloud->points.size ()      <<
+  " data points."                    <<
+  std::endl;
+  
+  
+  ne.setSearchMethod(tree);
+  ne.setInputCloud(objectCloud);
+  ne.setKSearch(25);
+  ne.compute(*normals);
+  
   
 	// Create the segmentation object for cylinder segmentation and set all the parameters
 	seg.setOptimizeCoefficients (true);
 	seg.setModelType (model);
   seg.setMethodType (SAC_RANSAC);
-  //seg.setMethodType(SAC_MLESAC);//terribly slow
   seg.setNormalDistanceWeight (0.1);
-  int objIterations = int(ceil(float(100)/iterationDivisor));
-	seg.setMaxIterations (objIterations);
+  int objIterations = int(ceil(float(10000)/iterationDivisor));
+	seg.setMaxIterations(objIterations);
 	seg.setDistanceThreshold (0.05);
 	seg.setRadiusLimits (0.01, 0.12);
-	seg.setInputCloud (cloudMinusObjectsAndPlane);
-	seg.setInputNormals (normalsMinusObjectsAndPlane);
+	seg.setInputCloud (objectCloud);
+	seg.setInputNormals (normals);
 	
 	// Obtain the cylinder inliers and coefficients
 	seg.segment (*inliers, *objectCoefficients);
 	std::cerr << "Object coefficients: " << *objectCoefficients << std::endl;
 	
-	extract.setInputCloud (cloudMinusObjectsAndPlane);
-	extract.setIndices (inliers);
-	extract.setNegative (false);
-  extract_normals.setIndices(inliers);
-	extract.filter(*cylinderCloud);
-  extract_normals.filter(*objectNormals);
+	extractor.setInputCloud (objectCloud);
+	extractor.setIndices (inliers);
+	extractor.setNegative (false);
+  normalExtractor.setIndices(inliers);
+	extractor.filter(*cylinderCloud);
+  normalExtractor.filter(*objectNormals);
   if (cylinderCloud->size() == 0){
     return false;
   } else {
     //subtract out the object just found from the original input cloud
-    extract.setInputCloud (cloudMinusObjectsAndPlane);
-    extract.setNegative (true);
-    extract.setIndices (inliers);
-    extract.filter (*cloudMinusObjectsAndPlane);
-    
-    extract_normals.setInputCloud (normalsMinusObjectsAndPlane);
-    extract_normals.setNegative (true);
-    extract_normals.setIndices(inliers);
-    extract_normals.filter (*normalsMinusObjectsAndPlane);
-
-    
+    extractor.setInputCloud (objectCloud);
+    extractor.setNegative (true);
+    extractor.setIndices (inliers);
+    extractor.filter (*objectCloud);        
     GraspableObject obj(*objectCoefficients,cylinderCloud,objectNormals);
     graspableObjects.push_back(obj);
     return true;
