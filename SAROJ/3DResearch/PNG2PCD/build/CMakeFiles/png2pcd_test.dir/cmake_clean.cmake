6d2f0013722230192ba6650274c8cf19d50d6124
sarojbardewa <saroj@pdx.edu>
2016-04-28 23:11:16 -0700

    Added png2pcd file

---
+++ SAROJ/3DResearch/PNG2PCD/build/CMakeFiles/png2pcd_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/png2pcd_test.dir/png2pcd.cpp.o"
+  "png2pcd_test.pdb"
+  "png2pcd_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/png2pcd_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
